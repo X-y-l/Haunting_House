d8647e58b477d9568b35ab4351d6743c7b0a0cb4
Zac Garby <me@zacgarby.co.uk>
2023-07-08 23:44:20 +0100

    drag n drop

--- Haunting_House/pin.gd
+++ Haunting_House/pin.gd
@@ -1,26 +1,48 @@
 extends RigidBody3D
 
+var placed: bool = false
+@export var drop_time: float = 5
+var reset_position = null
+var reset_rotation = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	reset_position = self.global_position
+	reset_rotation = self.global_rotation
 	self.freeze = true
-	$DropTimer.timeout.connect(_on_timer_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_timer_timeout():
+	drop()
+
+func _should_remove():
+	self.global_position = reset_position
+	self.global_rotation = reset_rotation
+	self.freeze = true
+	self.placed = false
+
+func place(object):
+	if placed:
+		return
+
+
+	self.freeze = true
+	self.placed = true
+	$DropTimer.start(drop_time)
+	print("placed for ", drop_time, " seconds in ", object)
+
+func drop():
 	self.freeze = false
 	
 	var rng = RandomNumberGenerator.new()
 	
-	self.rotate_x(rng.randf_range(0, 10))
-	self.rotate_y(rng.randf_range(0, 10))
-	self.rotate_z(rng.randf_range(0, 10))
+	self.apply_torque(Vector3(
+		rng.randf_range(-0.3, 0.3),
+		rng.randf_range(-0.3, 0.3),
+		rng.randf_range(-0.3, 0.3),
+	))
 	
 	$RemoveTimer.start(2)
-	$RemoveTimer.timeout.connect(_should_remove)
-
-func _should_remove():
-	self.queue_free()
