43aea4ce1e360b38570fc3de09e180f62e94e166
Zac Garby <me@zacgarby.co.uk>
2023-07-08 21:18:05 +0100

    back n forth

---
+++ Haunting_House/cctv.gd
@@ -0,0 +1,17 @@
+extends Camera3D
+
+var mid_rot: float
+var time: float = 0
+
+@export var move_speed = 0.5
+@export var move_amount = 0.2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mid_rot = self.rotation.y
+	time = hash(self.name)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta
+	self.rotation.y = mid_rot + (sin(time * move_speed) - 0.5) * move_amount * 2
