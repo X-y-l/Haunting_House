ce3b84a73eeefa232c5fe5017555ccf05eb3c611
Illin Tori <IllinTori@gmail.com>
2023-07-09 14:58:09 +0100

    Pin hover

--- Haunting_House/pin.gd
+++ Haunting_House/pin.gd
@@ -2,19 +2,26 @@ extends RigidBody3D
 
 var placed: bool = false
 @export var drop_time: float = 5
+@export var pin_name = "hello"
+@export var pin_hover = false
 var reset_position = null
 var reset_rotation = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	reset_position = self.global_position
-	reset_rotation = self.global_rotation + Vector3(0,3.14159,0)
+	reset_rotation = self.global_rotation + Vector3(3.14159,0,3.14159)
+	self.global_position = reset_position
+	self.global_rotation = reset_rotation
 	self.freeze = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if pin_hover:
+		$Sprite3D/SubViewport/Label.text = pin_name
+	else:
+		$Sprite3D/SubViewport/Label.text = ""
+		
 func _on_timer_timeout():
 	drop()
 
@@ -27,7 +34,6 @@ func _should_remove():
 func place(object):
 	if placed:
 		return
-
 
 	self.freeze = true
 	self.placed = true
